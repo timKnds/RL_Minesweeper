18641bfa30965189fd02e518d7303dccea7d2b71
timknudsen <tim04knudsen@gmail.com>
2024-03-31 21:02:13 +0200

    :bug:

--- model.py
+++ model.py
@@ -7,17 +7,20 @@ import torch.nn.functional as F
 class Linear_QNet(nn.Module):
     def __init__(self, input_channels, output_channels, shape):
         super().__init__()
-        # output_shape = (128, 6, 6)
-        self.conv1 = nn.Conv2d(input_channels, 128, kernel_size=3, stride=1, padding=0, bias=False)
+        # output_shape = (128, 16, 16)
+        self.conv1 = nn.Conv2d(input_channels, 128, kernel_size=5, stride=1, padding=2,
+                               padding_mode='replicate', bias=True)
         # add batch normalization
         self.batch_norm1 = nn.BatchNorm2d(128)
-        # output_shape = (128, 4, 4)
-        self.conv2 = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=0, bias=False)
+        # output_shape = (128, 14, 14)
+        self.conv2 = nn.Conv2d(128, 128, kernel_size=5, stride=1, padding=2,
+                               padding_mode='replicate', bias=False)
         self.batch_norm2 = nn.BatchNorm2d(128)
-        # output_shape = (64, 2, 2)
-        self.conv3 = nn.Conv2d(128, 64, kernel_size=3, stride=1, padding=0, bias=False)
+        # output_shape = (64, 13, 13)
+        self.conv3 = nn.Conv2d(128, 64, kernel_size=5, stride=1, padding=2,
+                               padding_mode='replicate', bias=False)
         self.batch_norm3 = nn.BatchNorm2d(64)
-        self.fc1 = nn.Linear((shape[0] - 6) * (shape[1] - 6) * 64, 512)
+        self.fc1 = nn.Linear(shape[0] * shape[1] * 64, 512)
         self.fc2 = nn.Linear(512, output_channels)
 
         # self.linear1 = nn.Linear(input_size, 4 * input_size)
@@ -34,7 +37,7 @@ class Linear_QNet(nn.Module):
         else:
             x = torch.flatten(x)
         x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
+        x = self.fc2(x)
         return x
 
     def save(self, name="rl_agent.pth"):
@@ -55,7 +58,7 @@ class QTrainer:
     def train_step(self, state, action, reward, next_state, done):
         state = torch.tensor(state, dtype=torch.float, device=self.device)
         next_state = torch.tensor(next_state, dtype=torch.float, device=self.device)
-        action = torch.tensor(action, dtype=torch.long, device=self.device)
+        # action = torch.tensor(action, dtype=torch.long, device=self.device)
         reward = torch.tensor(reward, dtype=torch.float, device=self.device)
 
         if len(state.shape) == 3:
@@ -64,25 +67,23 @@ class QTrainer:
             state = torch.unsqueeze(state, 0)
             next_state = torch.unsqueeze(next_state, 0)
             action = torch.unsqueeze(action, 0)
-        # else:
-        #     state = state.squeeze()
-        #     next_state = next_state.squeeze()
-        #     action = action.squeeze()
 
         # predict th
-        pred = self.model(state)
+        current_qs_list = self.model(state)
+        future_qs_list = self.model(next_state)
 
-        target = pred.clone()
+        target = current_qs_list.clone()
         for idx in range(len(done)):
-            Q_new = reward[idx]
             if not done[idx]:
-                Q_new = reward[idx] + self.gamma * torch.max(
-                    self.model(next_state[idx]))
+                max_future_q = torch.max(future_qs_list[idx])
+                Q_new = reward[idx] + self.gamma * max_future_q
+            else:
+                Q_new = reward[idx]
 
-            target[idx] = Q_new
+            target[idx][action[idx]] = Q_new
 
         self.optimizer.zero_grad()
-        loss = self.criterion(target, pred)
+        loss = self.criterion(target, current_qs_list)
         loss.backward()
 
         self.optimizer.step()
