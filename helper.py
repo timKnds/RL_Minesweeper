2d5f0c4098463121aa5ee8805f506fd213764e9f
timknudsen <tim04knudsen@gmail.com>
2024-03-31 21:03:31 +0200

    :poop:

--- helper.py
+++ helper.py
@@ -7,7 +7,7 @@ from IPython import display
 plt.ion()
 
 
-def plot(scores, mean_scores):
+def plot_score(scores, mean_scores):
     display.clear_output(wait=True)
     display.display(plt.gcf())
     plt.clf()
@@ -20,4 +20,13 @@ def plot(scores, mean_scores):
     plt.text(len(scores)-1, scores[-1], str(scores[-1]))
     plt.text(len(mean_scores)-1, mean_scores[-1], str(mean_scores[-1]))
     plt.show(block=True)
-    plt.pause(.1)
+    # plt.pause(.1)
+
+
+def plot_actions(actions):
+    display.clear_output(wait=True)
+    display.display(plt.gcf())
+    plt.clf()
+    plt.hist(actions, bins=256)
+    plt.show(block=True)
+    # plt.pause(.1)
