432feb11b25c1654bf37e3c1465bee3879c3cb0f
timknudsen <tim04knudsen@gmail.com>
2024-04-16 13:09:28 +0200

    :fire:

--- model.py
+++ model.py
@@ -4,53 +4,26 @@ import torch.nn.functional as F
 
 
 class Deep_QNet(nn.Module):
-    def __init__(self, input_channels, shape):
-        super().__init__()
-        # output_shape = (128, 16, 16)
-        self.conv1 = nn.Conv2d(input_channels, 128, kernel_size=3, stride=1, padding=1,
-                               padding_mode='zeros', bias=True)
-        # output_shape = (128, 14, 14)
-        self.conv2 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1,
-                               padding_mode='zeros', bias=True)
-        # output_shape = (64, 13, 13)
-        self.conv3 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1,
-                               padding_mode='zeros', bias=True)
-        self.conv4 = nn.Conv2d(256, 128, kernel_size=3, stride=1, padding=1,
-                               padding_mode='zeros', bias=True)
-        self.conv5 = nn.Conv2d(128, 64, kernel_size=3, stride=1, padding=1,
-                               padding_mode='zeros', bias=True)
-        self.conv6 = nn.Conv2d(64, 32, kernel_size=3, stride=1, padding=1,
-                               padding_mode='zeros', bias=True)
-        # self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=1)
-        self.fc1 = nn.Linear(shape[0] * shape[1] * 32, 512)
-        self.fc2 = nn.Linear(512, shape[0] * shape[1])
+    def __init__(self, nrows, ncols, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.conv1 = nn.Conv2d(1, 32, 5, padding=2)  # (batch, 32, +-0, +-0)
+        self.conv2 = nn.Conv2d(32, 128, 5, padding=2)  # (batch, 128, +-0, +-0)
 
-        # self.linear1 = nn.Linear(input_size, 4 * input_size)
-        # self.linear2 = nn.Linear(4 * input_size, 4 * output_size)
-        # self.linear3 = nn.Linear(4 * output_size, output_size)
+        self.fc1 = nn.Linear((128 * ncols * nrows) // 4, 128)
+        self.fc2 = nn.Linear(128, nrows * ncols)
 
     def forward(self, x):
         x = self.conv1(x)
         x = F.relu(x)
         x = self.conv2(x)
         x = F.relu(x)
-        x = self.conv3(x)
-        x = F.relu(x)
-        x = self.conv4(x)
-        x = F.relu(x)
-        x = self.conv5(x)
-        x = F.relu(x)
-        x = self.conv6(x)
-        x = F.relu(x)
-        # x = x.reshape(x.shape[0], -1)
-        if len(x.shape) == 4:
-            x = x.view(x.size(0), -1)
-        else:
-            x = torch.flatten(x)
+        x = F.max_pool2d(x, 2)
+        x = torch.flatten(x, 1)
         x = self.fc1(x)
         x = F.relu(x)
         x = self.fc2(x)
-        x = F.softmax(x, dim=1)
+        x = F.log_softmax(x, 1)
+
         return x
 
     def save(self, name="rl_agent.pth"):
